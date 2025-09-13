c0f9aea4eb9d1d680c1403df88db7dde3be74fdd
Guoyunf <38065115+Guoyunf@users.noreply.github.com>
2025-09-13 18:57:33 +0800

    Add primitive to extend arm forward-left

--- primitives/__init__.py
+++ primitives/__init__.py
@@ -5,6 +5,7 @@ from .grasp import grasp_handle
 from .pull import pull_handle_and_check, evaluate_joint3_effort
 from .retreat import retreat_gripper, retreat_base
 from .push import push_door
+from .extend import extend_forward_left
 
 __all__ = [
     "approach_handle",
@@ -15,4 +16,5 @@ __all__ = [
     "retreat_gripper",
     "retreat_base",
     "push_door",
+    "extend_forward_left",
 ]
